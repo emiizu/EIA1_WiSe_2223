eea9eb113457111a202a5a2a4ef0a480d670bc8b
emiizu <115719426+emiizu@users.noreply.github.com>
2022-12-01 14:34:41 +0100

    .

--- Aufgabe7/script.ts
+++ Aufgabe7/script.ts
@@ -12,15 +12,15 @@ console.log("DEUTSCHLAND");
     console.log("Die Gesamtzahl der Einwohnerinnen und Einwohner in Deutschland beträgt im Jahr 2022: " + EinwohnerDE + " Millionen");
 
     let EinwohnerEU: number = 447.7;
-    let EinProzentWert = EinwohnerEU / 100;
-    let DErelativzuEU = (EinwohnerDE / EinProzentWert).toFixed(2);
+    let EinProzentWert: number = Number(EinwohnerEU / 100);
+    let DErelativzuEU=(EinwohnerDE / EinProzentWert).toFixed(2);
     console.log("Relativ zur Gesamtzahl der Einwohner in der EU im Jahr 2022:  " + DErelativzuEU + "%");
 
     let EinwohnerDE08: number = 81.2;
-    let WachstumsrateDE = (((EinwohnerDE - EinwohnerDE08) / EinwohnerDE08) * 100).toFixed(2);
+    let WachstumsrateDE = Number(((EinwohnerDE - EinwohnerDE08) / EinwohnerDE08) * 100).toFixed(2);
     console.log("Wachstumsrate seit 2008: " + WachstumsrateDE + "%")
 
-    let WachstumDE = (EinwohnerDE - EinwohnerDE08).toFixed(2);
+    let WachstumDE = Number(EinwohnerDE - EinwohnerDE08).toFixed(2);
     console.log("Wachstum gesamt zwischen 2008 und 2022: " + WachstumDE + " Millionen");
 
 
@@ -34,14 +34,14 @@ let EinwohnerFR: number = 64.6;
 console.log("Die Gesamtzahl der Einwohnerinnen und Einwohner in Frankreich beträgt im Jahr 2022: " + EinwohnerFR + " Millionen");
 
 
-let FRrelativzuEU = (EinwohnerFR / EinProzentWert).toFixed(2);
+let FRrelativzuEU = Number(EinwohnerFR / EinProzentWert).toFixed(2);
 console.log("Relativ zur Gesamtzahl der Einwohner in der EU im Jahr 2022:  " + FRrelativzuEU + "%");
 
 let EinwohnerFR08: number = 61.7;
-let WachstumsrateFR = (((EinwohnerFR - EinwohnerFR08) / EinwohnerFR08) * 100).toFixed(2);
+let WachstumsrateFR = Number(((EinwohnerFR - EinwohnerFR08) / EinwohnerFR08) * 100).toFixed(2);
 console.log("Wachstumsrate seit 2008: " + WachstumsrateFR + "%")
 
-let WachstumFR = (EinwohnerFR - EinwohnerFR08).toFixed(2);
+let WachstumFR = Number(EinwohnerFR - EinwohnerFR08).toFixed(2);
 console.log("Wachstum gesamt zwischen 2008 und 2022: " + WachstumFR + " Millionen");
 
 
@@ -52,14 +52,14 @@ console.log("ITALIEN");
 let EinwohnerIT: number = 58.97;
 console.log("Die Gesamtzahl der Einwohnerinnen und Einwohner in Italien beträgt im Jahr 2022: " + EinwohnerIT + " Millionen");
 
-let ITrelativzuEU = (EinwohnerIT / EinProzentWert).toFixed(2);
+let ITrelativzuEU = Number(EinwohnerIT / EinProzentWert).toFixed(2);
 console.log("Relativ zur Gesamtzahl der Einwohner in der EU im Jahr 2022:  " + ITrelativzuEU + "%");
 
 let EinwohnerIT08: number = 59.02;
-let WachstumsrateIT = (((EinwohnerIT - EinwohnerIT08) / EinwohnerIT08) * 100).toFixed(2);
+let WachstumsrateIT = Number(((EinwohnerIT - EinwohnerIT08) / EinwohnerIT08) * 100).toFixed(2);
 console.log("Wachstumsrate seit 2008: " + WachstumsrateIT + "%")
 
-let WachstumIT = (EinwohnerIT - EinwohnerIT08).toFixed(2);
+let WachstumIT = Number(EinwohnerIT - EinwohnerIT08).toFixed(2);
 console.log("Wachstum gesamt zwischen 2008 und 2022: " + WachstumIT + " Millionen");
 
 
@@ -68,87 +68,69 @@ console.log("POLEN");
 let EinwohnerPL: number = 41.09;
 console.log("Die Gesamtzahl der Einwohnerinnen und Einwohner in Polen beträgt im Jahr 2022: " + EinwohnerPL + " Millionen");
 
-let PLrelativzuEU = (EinwohnerPL / EinProzentWert).toFixed(2);
+let PLrelativzuEU = Number(EinwohnerPL / EinProzentWert).toFixed(2);
 console.log("Relativ zur Gesamtzahl der Einwohner in der EU im Jahr 2022:  " + PLrelativzuEU + "%");
 
 let EinwohnerPL08: number = 38.5;
-let WachstumsratePL = (((EinwohnerPL - EinwohnerPL08) / EinwohnerPL08) * 100).toFixed(2);
+let WachstumsratePL = Number(((EinwohnerPL - EinwohnerPL08) / EinwohnerPL08) * 100).toFixed(2);
 console.log("Wachstumsrate seit 2008: " + WachstumsratePL + "%")
 
-let WachstumPL = (EinwohnerPL - EinwohnerPL08).toFixed(2);
+let WachstumPL = Number(EinwohnerPL - EinwohnerPL08).toFixed(2);
 console.log("Wachstum gesamt zwischen 2008 und 2022: " + WachstumPL + " Millionen");
 
 console.log("EU");
 
 
 let EinwohnerEU08: number = 437.44;
-let WachstumsrateEU = (((EinwohnerEU - EinwohnerEU08) / EinwohnerEU08) * 100).toFixed(2);
+let WachstumsrateEU = Number(((EinwohnerEU - EinwohnerEU08) / EinwohnerEU08) * 100).toFixed(2);
 console.log("Wachstumsrate seit 2008: " + WachstumsrateEU + "%")
 
-let WachstumEU = (EinwohnerEU - EinwohnerEU08).toFixed(2);
+let WachstumEU = Number(EinwohnerEU - EinwohnerEU08).toFixed(2);
 console.log("Wachstum gesamt zwischen 2008 und 2022: " + WachstumEU + " Millionen");
 
-function myFunction (Relativ:number, Einwohner:number, Wachstumsrate:number, Wachstum:number) {
+function myFunction (Relativ:string, Einwohner:number, Wachstumsrate:string, Wachstum:string) {
 
     document.querySelector(".chart").setAttribute("style","height:" + Relativ + "%");
     document.querySelector("#Einwohner").innerHTML = Einwohner + " Mio";
     document.querySelector("#Relativ").innerHTML = Relativ + " %";
     document.querySelector("#Wachstumsrate").innerHTML = Wachstumsrate + " %";
     document.querySelector("#Wachstum").innerHTML = Wachstum + " Mio";
+  
     
    
 
 }
 
-document.querySelector("#de").addEventListener('click', myFunction(DErelativzuEU, EinwohnerDE, WachstumsrateDE, WachstumDE))
+document.querySelector("#de").addEventListener('click', function () {
+     myFunction(DErelativzuEU, EinwohnerDE, WachstumsrateDE, WachstumDE);
+     document.querySelector("#title").innerHTML ="Einwohnerzahlen in Deutschland";
+     document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Deutschland in 2022";
+});
 
-document.querySelector("#fr").addEventListener('click', function() {
-
-    document.querySelector(".chart").setAttribute("style","height:" + FRrelativzuEU + "%");
-    document.querySelector("#Einwohner").innerHTML = EinwohnerFR + " Mio";
-    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Frankreich in 2022";
-    document.querySelector("#Relativ").innerHTML = FRrelativzuEU + " %";
-    document.querySelector("#Wachstumsrate").innerHTML = WachstumsrateFR + " %";
-    document.querySelector("#Wachstum").innerHTML = WachstumFR + " Mio";
+document.querySelector("#fr").addEventListener('click', function () {
+    myFunction(FRrelativzuEU, EinwohnerFR, WachstumsrateFR, WachstumFR);
     document.querySelector("#title").innerHTML ="Einwohnerzahlen in Frankreich";
+    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Frankreich in 2022";
+});
 
-})
-
-document.querySelector("#it").addEventListener('click', function() {
-
-    document.querySelector(".chart").setAttribute("style","height:" + ITrelativzuEU + "%");
-    document.querySelector("#Einwohner").innerHTML = EinwohnerIT + " Mio";
-    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Italien in 2022";
-    document.querySelector("#Relativ").innerHTML = ITrelativzuEU + " %";
-    document.querySelector("#Wachstumsrate").innerHTML = WachstumsrateIT + " %";
-    document.querySelector("#Wachstum").innerHTML = WachstumIT + " Mio";
+document.querySelector("#it").addEventListener('click', function () {
+    myFunction(ITrelativzuEU, EinwohnerIT, WachstumsrateIT, WachstumIT);
     document.querySelector("#title").innerHTML ="Einwohnerzahlen in Italien";
+    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Italien in 2022";
+});
 
-})
-
-document.querySelector("#pl").addEventListener('click', function() {
-
-    document.querySelector(".chart").setAttribute("style","height:" + PLrelativzuEU + "%");
-    document.querySelector("#Einwohner").innerHTML = EinwohnerPL + " Mio";
-    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Polen in 2022";
-    document.querySelector("#Relativ").innerHTML = PLrelativzuEU + " %";
-    document.querySelector("#Wachstumsrate").innerHTML = WachstumsratePL + " %";
-    document.querySelector("#Wachstum").innerHTML = WachstumPL + " Mio";
+document.querySelector("#pl").addEventListener('click', function () {
+    myFunction(PLrelativzuEU, EinwohnerPL, WachstumsratePL, WachstumPL);
     document.querySelector("#title").innerHTML ="Einwohnerzahlen in Polen";
+    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in Polen in 2022";
+});
 
-})
-
-document.querySelector("#eu").addEventListener('click', function() {
-
-    document.querySelector(".chart").setAttribute("style","height:100%");
-    document.querySelector("#Einwohner").innerHTML = EinwohnerEU + " Mio";
-    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in der Europäischen Union in 2022";
-    document.querySelector("#Relativ").innerHTML = "100%";
-    document.querySelector("#Wachstumsrate").innerHTML = WachstumsrateEU + " %";
-    document.querySelector("#Wachstum").innerHTML = WachstumEU + " Mio";
+document.querySelector("#eu").addEventListener('click', function () {
+    myFunction("100%", EinwohnerEU, WachstumsrateEU, WachstumEU);
     document.querySelector("#title").innerHTML ="Einwohnerzahlen in der Europäischen Union";
+    document.querySelector("#EinwohnerText").innerHTML = "Gesamtzahl Einwohnerinnen und Einwohner in der Europäischen Union in 2022";
+});
 
-})
 
 
 }
