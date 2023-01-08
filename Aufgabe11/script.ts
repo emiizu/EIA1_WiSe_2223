55f805c3dc3710fb0dd55367795870de4a1ebc61
emiizu <115719426+emiizu@users.noreply.github.com>
2023-01-09 00:13:50 +0100

    11

--- Aufgabe11/script.ts
+++ Aufgabe11/script.ts
@@ -1,72 +1,74 @@
-window.addEventListener("load", function (): void {
+namespace L11 {
+    window.addEventListener("load", function (): void {
 
 
     
-      let enter: HTMLInputElement = (document.getElementById("inputText") as HTMLInputElement);
-      let tasksNumber: number = 0;
-   
-      enter.addEventListener("keypress", function (event: KeyboardEvent) {
-          if (event.key == "Enter") {
-               addTask();
-               enter.value = "";
-          }
-      });
-   
-   
-   
-      
-   
-   
-      function addTask() {
-   
-          
-   
-          let div: HTMLDivElement = document.createElement("div");
-          div.className = "toDoItem";
-   
-          
-          let checkbox: HTMLInputElement = document.createElement("input");
-          checkbox.type = "checkbox";
-          checkbox.className = "checkBox";
-   
-          let label: HTMLLabelElement = document.createElement("label");
-          label.innerHTML = enter.value;
-          label.className = "taskText"; 
-   
-          let trash: HTMLElement = document.createElement("i");
-          trash.className = "far fa-trash-alt"; 
-   
-          document.getElementById("toDoList").appendChild(div);
-          div.appendChild(checkbox);
-          div.appendChild(label);
-          div.appendChild(trash);
-   
-          function deleteTask(div: HTMLDivElement) {
-            div.remove();
+        let enter: HTMLInputElement = (document.getElementById("inputText") as HTMLInputElement);
+        let tasksNumber: number = 0;
      
-            tasksNumber--;
+        enter.addEventListener("keypress", function (event: KeyboardEvent) {
+            if (event.key == "Enter") {
+                 addTask();
+                 enter.value = "";
+            }
+        });
+     
+     
+     
+        
+     
+     
+        function addTask() {
+     
+            
+     
+            let div: HTMLDivElement = document.createElement("div");
+            div.className = "toDoItem";
+     
+            
+            let checkbox: HTMLInputElement = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.className = "checkBox";
+     
+            let label: HTMLLabelElement = document.createElement("label");
+            label.innerHTML = enter.value;
+            label.className = "taskText"; 
+     
+            let trash: HTMLElement = document.createElement("i");
+            trash.className = "far fa-trash-alt"; 
+     
+            document.getElementById("toDoList").appendChild(div);
+            div.appendChild(checkbox);
+            div.appendChild(label);
+            div.appendChild(trash);
+     
+            function deleteTask(div: HTMLDivElement) {
+              div.remove();
+       
+              tasksNumber--;
+              tasksAnzahl();
+          }
+            trash.addEventListener("click", function (event: MouseEvent) {
+                console.log(event);
+                deleteTask(div);
+                
+            });
+     
+            tasksNumber++;
             tasksAnzahl();
         }
-          trash.addEventListener("click", function (event: MouseEvent) {
-              console.log(event);
-              deleteTask(div);
-              
-          });
-   
-          tasksNumber++;
-          tasksAnzahl();
-      }
-   
-   
-   
-      function tasksAnzahl() {
-          document.getElementById("counter").innerText = tasksNumber + " in total";
-      }
-   
-   
-   
-   
-   
-   
-   
-   });
+     
+     
+     
+        function tasksAnzahl() {
+            document.getElementById("counter").innerText = tasksNumber + " in total";
+        }
+      // ich war am donnerstag und über die ferien krank und habe die aufgabe nicht erledigt, ich mache es aber bis zum donnerstag. tut mir leid...//
+     
+     
+     
+     
+     
+     
+     });
+}
